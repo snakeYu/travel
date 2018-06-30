5b2cc5e06574b41936d939d0c0e13c66f60a6e9e
snakeYu <897910109@qq.com>
2018-06-30 23:17:11 +0800

    city-search done

---
+++ src/page/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市名或拼音">
+  </div>
+</template>
+<script>
+export default{
+  name: 'citySearch'
+}
+</script>
+<style lang="scss" scoped>
+@import '~@/assets/styles/varibles.scss';
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background-color: $bgColor;
+  .search-input {
+    box-sizing: border-box;
+    height: 0.62rem;
+    line-height: 0.62rem;
+    width: 100%;
+    text-align: center;
+    border-radius: 0.06rem;
+    color: #666;
+    padding: 0 0.1rem;
+  }
+}
+</style>
